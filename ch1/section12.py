6cccbf07f44facdd75aae4ec860926f4ed62e85c
paloblanco <45581243+paloblanco@users.noreply.github.com>
2020-06-15 13:36:17 -0400

    implemented bit class

--- ch1/section12.py
+++ ch1/section12.py
@@ -1,40 +1,43 @@
 from typing import Union
 
-class GetList:
-    def __init__(self, startlist: list = []):
-        self._list = startlist
-
-    def __getitem__(self, key):
-        return self._list[key]
-
-class BitString(int):
-    def __new__(cls, value: Union[int,str] = 0) -> None:
+class BitString:
+    def __init__(self, value: Union[int,str] = 0) -> None:
         if type(value) == int:
-            return int.__new__(cls, int(value))
+            self._value: int = value
         elif type(value) == str:
             try:
                 int_val = int(value,2)
-                return int.__new__(cls, int(int_val))
+                self._value = int_val
             except ValueError:
-                print("String must be sequence of 0 and 1, ie '0101'")
+                print("String must be sequence of 0 and 1, eg '0101'")
+     
+    def append_bit_string(self, value: str) -> str:   
+        try:
+            int_new_value: int = int(value,2)
+            length_bits = len(value) # need length from string, in case there are leading 0s
+            self._value <<= length_bits
+            self._value |= int_new_value
+            return format(self._value, "b")
+        except ValueError:
+            print("String must be sequence of 0 and 1, ie '0101'")
     
+    def get_length(self) -> int:
+        return len(format(self._value,"b"))
+
     def __getitem__(self, key: int) -> str:
-        value_bit_string: str = format(self, "b")
+        value_bit_string: str = format(self._value, "b")
         return value_bit_string[key]
 
     def __repr__(self) -> str:
-        return repr(format(self, "b"))
+        return format(self._value, "b")
 
-    def append_bit_string(self, value: str) -> str:   
-        try:
-            int_new_value = int(value,2)
-            length_bits = int_new_value.bit_length()
-            value_new_int = int(value_new_string,2)
-            self = int.__new__(BitString, int(value_new_int))
-            return format(self, "b")
-        except ValueError:
-            print("String must be sequence of 0 and 1, ie '0101'")
 
+def test_BitString_get_length():
+    test_int = 107
+    test_string_old = '1101011'
+    test_len = len(test_string_old)
+    bit = BitString(107)
+    assert bit.get_length() == test_len
 
 def test_BitString_append_val():
     test_int = 107
@@ -43,7 +46,6 @@ def test_BitString_append_val():
     new_val = 429
     bit = BitString(test_int)
     assert bit.append_bit_string(test_val) == test_string_old+test_val
-    assert bit + 0 == new_val
 
 def test_BitString_get_5():
     test_int = 127
@@ -60,42 +62,48 @@ def test_BitString_get_slice():
     bit = BitString(test_int)
     assert bit[slice_start:slice_end] == expected_value
 
+def run_tests():
+    test_BitString_append_val()
+    test_BitString_get_5()
+    test_BitString_get_length()
+    test_BitString_get_slice()
 
 class CompressedGene:
     def __init__(self, gene: str) -> None:
         self._compress(gene)
 
     def _compress(self, gene: str) -> None:
-        self.bit_string: int = 1
         for nucleotide in gene.upper():
-            self.bit_string <<= 2
             if nucleotide == "A":
-                self.bit_string |= 0b00
+                bit_to_set = ("00")
             elif nucleotide == "C":
-                self.bit_string |= 0b01
+                bit_to_set = ("01")
             elif nucleotide == "G":
-                self.bit_string |= 0b10
+                bit_to_set = ("10")
             elif nucleotide == "T":
-                self.bit_string |= 0b11
+                bit_to_set = ("11")
             else:
                 raise ValueError(f"Invalid Nuecleotide:{nucleotide}")
+            try:
+                self.bit_string.append_bit_string(bit_to_set)
+            except:
+                self.bit_string: BitString = BitString(bit_to_set)
 
     def decompress(self) -> str:
         gene: str = ""
-        for i in range(0, self.bit_string.bit_length() - 1, 2):
-            bits: int = self.bit_string >> i & 0b11
-            if bits == 0b00:
+        for i in range(0, self.bit_string.get_length(), 2):
+            bits: str = self.bit_string[i:i+2]
+            if bits == "00":
                 gene += "A"
-            elif bits == 0b01:
+            elif bits == "01":
                 gene += "C"
-            elif bits == 0b10:
+            elif bits == "10":
                 gene += "G"
-            elif bits == 0b11:
+            elif bits == "11":
                 gene += "T"
             else:
-                raise ValueError(f"Invalid bits:{bits}"
-            )
-        return gene[::-1]
+                raise ValueError(f"Invalid bits:{bits}")
+        return gene
 
     def __str__(self) -> str:
         return self.decompress()
@@ -103,6 +111,7 @@ class CompressedGene:
 
 
 if __name__ == "__main__":
+    run_tests()
     from sys import getsizeof
     original :str = "TAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATATAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATA" * 100
     print(f"original is {getsizeof(original)} bytes")
